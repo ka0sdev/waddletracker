157c8ef982e5bb844457a80c90aaedc5d0763954
ka0sdev <16397298+ka0sdev@users.noreply.github.com>
2026-08-12 20:53:03 +0200

    feat: Add StreakStatistics interface and refactor HistoricalStatistics for improved clarity

--- src/types/StatisticsTypes.ts
+++ src/types/StatisticsTypes.ts
@@ -20,14 +20,42 @@ export interface BestDayStatistics {
   activeMilliseconds: number;
 }
 
+export interface StreakStatistics {
+  currentDays: number;
+  longestDays: number;
+
+  currentStartDate:
+    | string
+    | undefined;
+
+  currentEndDate:
+    | string
+    | undefined;
+
+  longestStartDate:
+    | string
+    | undefined;
+
+  longestEndDate:
+    | string
+    | undefined;
+}
+
 export interface HistoricalStatistics {
   range: StatisticsRange;
 
-  startDate: string | undefined;
-  endDate: string | undefined;
+  startDate:
+    | string
+    | undefined;
+
+  endDate:
+    | string
+    | undefined;
 
   activeMilliseconds: number;
+
   activeDays: number;
+
   averageActiveMilliseconds: number;
 
   bestDay:
@@ -35,7 +63,9 @@ export interface HistoricalStatistics {
     | undefined;
 
   projects: StatisticsDimension[];
+
   languages: StatisticsDimension[];
+
   files: StatisticsDimension[];
 
   daily: DailyActivityPoint[];
